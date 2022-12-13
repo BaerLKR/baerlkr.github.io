41965850f70d0b798a8d053c8328fc22c7f39c93
BaerLKR <83340886+BaerLKR@users.noreply.github.com>
2022-12-13 19:13:52 +0100

    2.7.0 + geschichte Q1.2

---
+++ js/initnotiv.js
@@ -0,0 +1,7 @@
+const wilkommen = 'Welcome to my corner of the internet! <br>' + 
+'If you have any suggestions or issues please contact me <br>' +
+'<a class="a" href="versions.txt">v2.7.0</a>'
+function initnotiv() {
+    //wilkommensnachricht
+    notiv(wilkommen)
+}
