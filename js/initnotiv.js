d6d6652f3e644afcbe82397552c5b051eb5d4059
BaerLKR <83340886+BaerLKR@users.noreply.github.com>
2022-12-17 19:20:59 +0100

    2.8.1

--- js/initnotiv.js
+++ js/initnotiv.js
@@ -1,6 +1,6 @@
 const wilkommen = 'Welcome to my corner of the internet! <br>' + 
 'If you have any suggestions or issues please contact me <br>' +
-'<a class="a" href="versions.txt">v2.7.4</a>'
+'<a class="a" href="versions.txt">v2.8.0</a>'
 function initnotiv() {
     //wilkommensnachricht
     notiv(wilkommen)
