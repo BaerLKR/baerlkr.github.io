444af6eacee0eaa8be6e063984b25fce323006b8
Lovis Rentsch <gpg@lovirent.eu>
2023-04-15 17:37:02 +0200

    commandline push

--- js/initnotiv.js
+++
@@ -1,7 +0,0 @@
-const wilkommen = 'Welcome to my corner of the internet! <br>' + 
-'If you have any suggestions or issues please contact me <br>' +
-'<a class="a" href="versions.txt">v2.8.2</a>'
-function initnotiv() {
-    //wilkommensnachricht
-    notiv(wilkommen)
-}
