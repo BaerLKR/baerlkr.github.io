b68411350dd50f58267085b08991ea30909f3d81
Lovis Rentsch <gpg@lovirent.eu>
2024-07-03 22:18:42 +0200

    animation lock

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 #![recursion_limit = "1024"]
 
+use std::cell::RefCell;
+use std::rc::Rc;
+
 use console_error_panic_hook::set_once as set_panic_hook;
 use wasm_bindgen::prelude::*;
 use wasm_bindgen::JsCast;
@@ -7,26 +10,27 @@ use web_sys::window;
 use web_sys::HtmlElement;
 
 const LENGTH: usize = ALPHABET.len();
-const ALPHABET: &str = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+const ALPHABET: &str =
+    "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890#$%&/()=?!+*~";
 const NAME: &str = "Lovis Rentsch";
 
 #[wasm_bindgen]
 extern "C" {
     #[wasm_bindgen(js_namespace = Math)]
     fn random() -> f32;
+    #[wasm_bindgen(js_namespace = console)]
+    fn log(message: &str);
 }
 
-fn start_app() -> Result<(), JsValue> {
-    let document = window()
-        .and_then(|win| win.document())
-        .expect("Could not access document");
+fn cursor_glow() {
+    let js_window = window().unwrap();
+    let document = js_window.document().unwrap();
 
     let div = document
         .get_element_by_id("follower")
         .expect("element should exist")
         .dyn_into::<HtmlElement>()
         .unwrap();
-    let name_element = document.get_element_by_id("name").unwrap();
     let closure = Closure::wrap(Box::new(move |event: web_sys::MouseEvent| {
         let height = div.scroll_height();
         let x = event.client_x();
@@ -38,6 +42,17 @@ fn start_app() -> Result<(), JsValue> {
             .set_property("top", &format!("{}px", y - height / 2))
             .unwrap();
     }) as Box<dyn FnMut(_)>);
+    js_window
+        .add_event_listener_with_callback("mousemove", closure.as_ref().unchecked_ref())
+        .unwrap();
+    closure.forget();
+}
+fn text_animation() {
+    const ANIMATION_INTERVAL: i32 = 120;
+    let animation_ongoing = Rc::new(RefCell::new(false));
+    let js_window = window().unwrap();
+    let document = js_window.document().unwrap();
+    let name_element = document.get_element_by_id("name").unwrap();
     let anim = Closure::wrap(Box::new(move |iteration: usize, element: HtmlElement| {
         element.set_text_content(Some({
             &NAME
@@ -58,52 +73,51 @@ fn start_app() -> Result<(), JsValue> {
                 .collect::<String>()
         }));
     }) as Box<dyn FnMut(_, _)>);
-    let name_effect = Closure::wrap(Box::new(move |_event: web_sys::MouseEvent| {
-        let window = window().unwrap();
-        let elem = document
-            .get_element_by_id("name")
-            .unwrap()
-            .dyn_into::<HtmlElement>()
-            .unwrap();
-        for i in 0..(NAME.len() as i32 * 2) {
-            window
-                .set_timeout_with_callback_and_timeout_and_arguments_2(
-                    anim.as_ref().unchecked_ref(),
-                    i * 100,
-                    &i.into(),
-                    &elem.to_owned(),
-                )
-                .unwrap();
-        }
-    }) as Box<dyn FnMut(_)>);
-    let window = window().unwrap();
-    window.add_event_listener_with_callback("mousemove", closure.as_ref().unchecked_ref())?;
-    name_element
-        .add_event_listener_with_callback("mouseover", name_effect.as_ref().unchecked_ref())?;
-    name_effect.forget();
-    closure.forget();
-    Ok(())
-}
-fn anim(element: &mut HtmlElement, iteration: &usize) {
-    element.set_text_content(Some({
-        &NAME
-            .chars()
-            .enumerate()
-            .map(|(i, c)| {
-                if i < *iteration {
-                    ALPHABET
-                        .chars()
-                        .nth((random() * LENGTH as f32) as usize)
-                        .unwrap()
-                } else {
-                    c
+    let end_animation = Closure::wrap({
+        let animation_ongoing = animation_ongoing.clone();
+        Box::new(move || {
+            animation_ongoing.replace(false);
+        }) as Box<dyn FnMut()>
+    });
+    let name_effect = Closure::wrap({
+        let animation_ongoing = animation_ongoing.clone();
+        Box::new(move |_event: web_sys::MouseEvent| {
+            if *animation_ongoing == false.into() {
+                animation_ongoing.replace(true);
+                let window = window().unwrap();
+                let elem = document
+                    .get_element_by_id("name")
+                    .unwrap()
+                    .dyn_into::<HtmlElement>()
+                    .unwrap();
+                window
+                    .set_timeout_with_callback_and_timeout_and_arguments_0(
+                        end_animation.as_ref().unchecked_ref(),
+                        (NAME.len() as i32 * 2) * ANIMATION_INTERVAL,
+                    )
+                    .unwrap();
+                for i in 0..(NAME.len() as i32 * 2) {
+                    window
+                        .set_timeout_with_callback_and_timeout_and_arguments_2(
+                            anim.as_ref().unchecked_ref(),
+                            i * ANIMATION_INTERVAL,
+                            &i.into(),
+                            &elem.to_owned(),
+                        )
+                        .unwrap();
                 }
-            })
-            .collect::<String>()
-    }))
+            }
+        }) as Box<dyn FnMut(_)>
+    });
+
+    name_element
+        .add_event_listener_with_callback("mouseover", name_effect.as_ref().unchecked_ref())
+        .unwrap();
+    name_effect.forget();
 }
 
 fn main() {
     set_panic_hook();
-    start_app().unwrap();
+    text_animation();
+    cursor_glow()
 }
