1790d5d6a7337c5d4503aeee6c7d527eaf0d8688
Lovis Rentsch <gpg@lovirent.eu>
2024-09-07 17:54:02 +0200

    added list for english rss feed

--- src/blog.rs
+++ src/blog.rs
@@ -4,6 +4,13 @@ pub async fn blog() {
     let win = window().unwrap();
     let bod = win.document().unwrap();
     let feed_element = bod.get_element_by_id("blog-feed").unwrap();
+    let feed_element_en = bod.get_element_by_id("blog-feed-en").unwrap();
+    let feed_en = reqwest::get("https://blog.lovirent.eu/en/index.xml")
+        .await
+        .unwrap()
+        .bytes()
+        .await
+        .unwrap();
     let feed = reqwest::get("https://blog.lovirent.eu/index.xml")
         .await
         .unwrap()
@@ -11,6 +18,18 @@ pub async fn blog() {
         .await
         .unwrap();
     let channel = rss::Channel::read_from(&feed[..]).unwrap();
+    let channel_en = rss::Channel::read_from(&feed_en[..]).unwrap();
+    let cont_en = channel_en
+        .items()
+        .iter()
+        .map(|item| {
+            format!(
+                "<li><a href='{}'>{}</a></li>",
+                item.link().unwrap(),
+                item.title().unwrap()
+            )
+        })
+        .collect::<String>();
     let cont = channel
         .items()
         .iter()
@@ -22,6 +41,11 @@ pub async fn blog() {
             )
         })
         .collect::<String>();
+    if cont_en.is_empty() {
+        feed_element.set_text_content(Some("no posts yet"));
+    } else {
+        feed_element_en.set_inner_html(&cont_en);
+    }
     if cont.is_empty() {
         feed_element.set_text_content(Some("no posts yet"));
     } else {
